2abbf0586428f77b340e54ae68c572dbc38d110e
Eliseo Casas Bossicovich <beliseocasas@gmail.com>
2024-02-13 18:37:16 -0300

    json server installed, login styled

--- src/app/layouts/auth/auth-routing.module.ts
+++ src/app/layouts/auth/auth-routing.module.ts
@@ -2,10 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 
-// /auth
 const routes: Routes = [
   {
-    // /auth/login
     path: 'login',
     component: LoginComponent,
   },
@@ -16,4 +14,3 @@ const routes: Routes = [
   exports: [RouterModule],
 })
 export class AuthRoutingModule {}
-// 
