cbe8b7a709315d184b738f3df04fd2036ec17e56
Eliseo Casas Bossicovich <beliseocasas@gmail.com>
2024-03-04 19:01:10 -0300

    shared modified

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -11,6 +11,13 @@ import { LoginValidationErrorsPipe } from './pipes/login-validation-errors.pipe'
 import { CalculateAgePipe } from './pipes/calculate-age.pipe';
 import { titleStyleDirective } from './directives/title-style.directive';
 import { FullNamePipe } from './pipes/full-name.pipe';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { FullNameInscriptionsPipe } from './pipes/full-name-enrollment.pipe';
 
 @NgModule({
   declarations: [
@@ -18,18 +25,25 @@ import { FullNamePipe } from './pipes/full-name.pipe';
     CalculateAgePipe,
     titleStyleDirective,
     LoginValidationErrorsPipe,
+    FullNameInscriptionsPipe,
   ],
   imports: [CommonModule],
   exports: [
     FullNamePipe,
     CalculateAgePipe,
-    titleStyleDirective,
-    MatCardModule,
-    MatFormFieldModule,
-    ReactiveFormsModule,
-    LoginValidationErrorsPipe,
+    MatTableModule,
     MatIconModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
     MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatCardModule,
+    LoginValidationErrorsPipe,
+    FullNameInscriptionsPipe,
   ],
 })
 export class SharedModule {}
